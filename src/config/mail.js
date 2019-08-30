f285e4bd6041fb64eb1998e211ae0d190eee353f
johnny <j.vitor.mf@gmail.com>
2019-08-29 23:38:13 -0300

    express-handlebars nodemailer-express-handlebars

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: '7267e2fd06b5c9',
+        pass: '60d54950188b57',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
